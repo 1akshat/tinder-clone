62c54135eb92887b7d22d239b8ad7bdcc6deab8b
1akshat <akshat.akshat6@gmail.com>
2020-08-16 13:01:08 +0530

    load data dynamically from firestore db

--- src/Components/HomePage/HomePage.js
+++ src/Components/HomePage/HomePage.js
@@ -1,40 +1,20 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import TinderCard from "react-tinder-card";
+import database from "../../firebase";
 import "./HomePage.css";
 
 const HomePage = () => {
-  const [people, setPeople] = useState([
-    {
-      name: "Michel Joe",
-      url:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQxhRWVBVBcIINFDrTuCSNwm-QsnBOpOn3ybQ&usqp=CAU",
-    },
-    {
-      name: "David Gandy",
-      url:
-        "https://manofmany.com/wp-content/uploads/2019/03/10-Most-Famous-Male-Models-of-All-Time-David-Gandy.jpg",
-    },
-    {
-      name: "Mark Shawn",
-      url:
-        "https://c6oxm85c.cloudimg.io/cdno/n/q85/https://az617363.vo.msecnd.net/imgmodels/models/MD30001159/andreasdp119382a9c9d8dc05340a075edf137e150_thumb.jpg",
-    },
-    {
-      name: "Sawn Joe",
-      url:
-        "https://mediaslide-europe.storage.googleapis.com/named/pictures/768/1853/profile-1566992424-96f5a1800431801f3d48e0f68e506abe.jpg?v=1566992565",
-    },
-    {
-      name: "Bela Hedid",
-      url:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRLC82Stho0GRuZd4XIUf73yeP1sM0UlYpIRA&usqp=CAU",
-    },
-    {
-      name: "Mini Dsouza",
-      url:
-        "https://www.allperfectstories.com/wp-content/uploads/2017/06/MODEL-1024x640.jpg",
-    },
-  ]);
+  const [people, setPeople] = useState([]);
+
+  useEffect(() => {
+    function fetchPeople() {
+      database.collection("people").onSnapshot((snapshot) => {
+        setPeople(snapshot.docs.map((doc) => doc.data()));
+      });
+    }
+
+    fetchPeople();
+  }, []);
 
   return (
     <div>
