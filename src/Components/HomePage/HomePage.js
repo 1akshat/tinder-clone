1efcd7fc77fc483c90732af7243ccef20aa27268
1akshat <akshat.akshat6@gmail.com>
2020-08-16 13:10:17 +0530

    refactor components structure

--- src/Components/HomePage/HomePage.js
+++ src/Components/HomePage/HomePage.js
@@ -1,42 +1,11 @@
-import React, { useState, useEffect } from "react";
-import TinderCard from "react-tinder-card";
-import database from "../../firebase";
-import "./HomePage.css";
+import React from "react";
+import TinderCards from "./TinderCards/TinderCards";
 
 const HomePage = () => {
-  const [people, setPeople] = useState([]);
-
-  useEffect(() => {
-    function fetchPeople() {
-      database.collection("people").onSnapshot((snapshot) => {
-        setPeople(snapshot.docs.map((doc) => doc.data()));
-      });
-    }
-
-    fetchPeople();
-  }, []);
-
   return (
-    <div>
-      <div className="card-container">
-        {people.map((person) => (
-          <TinderCard
-            className="swipe"
-            key={person.name}
-            preventSwipe={["up", "down"]}
-          >
-            <div
-              className="card"
-              style={{
-                backgroundImage: `url(${person.url})`,
-              }}
-            >
-              <h3>{person.name}</h3>
-            </div>
-          </TinderCard>
-        ))}
-      </div>
-    </div>
+    <>
+      <TinderCards />;
+    </>
   );
 };
 
---
+++ src/Components/HomePage/TinderCards/TinderCards.js
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from "react";
+import TinderCard from "react-tinder-card";
+import "./TinderCards.css";
+import database from "../../../firebase";
+
+const TinderCards = () => {
+  const [people, setPeople] = useState([]);
+
+  useEffect(() => {
+    function fetchPeople() {
+      database.collection("people").onSnapshot((snapshot) => {
+        setPeople(snapshot.docs.map((doc) => doc.data()));
+      });
+    }
+
+    fetchPeople();
+  }, []);
+
+  return (
+    <div>
+      <div className="card-container">
+        {people.map((person) => (
+          <TinderCard
+            className="swipe"
+            key={person.name}
+            preventSwipe={["up", "down"]}
+          >
+            <div
+              className="card"
+              style={{
+                backgroundImage: `url(${person.url})`,
+              }}
+            >
+              <h3>{person.name}</h3>
+            </div>
+          </TinderCard>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default TinderCards;
