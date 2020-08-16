672d81d523ff768facc03c211e7ea4610fe69efd
1akshat <akshat.akshat6@gmail.com>
2020-08-16 19:23:38 +0530

    final routing from chat to chat-screen

--- src/Components/ChatPage/Chat/Chat.js
+++ src/Components/ChatPage/Chat/Chat.js
@@ -1,17 +1,20 @@
 import React from "react";
 import Avatar from "@material-ui/core/Avatar";
+import { Link } from "react-router-dom";
 import "./Chat.css";
 
 const Chat = ({ name, message, timestamp, profilePicture }) => {
   return (
-    <div className="chat">
-      <Avatar className="chat-image" alt={name} src={profilePicture}></Avatar>
-      <div className="chat-content">
-        <h2>{name}</h2>
-        <p>{message}</p>
+    <Link to={`chat/${name}`}>
+      <div className="chat">
+        <Avatar className="chat-image" alt={name} src={profilePicture}></Avatar>
+        <div className="chat-content">
+          <h2>{name}</h2>
+          <p>{message}</p>
+        </div>
+        <p className="chat-timestamp">{timestamp}</p>
       </div>
-      <p className="chat-timestamp">{timestamp}</p>
-    </div>
+    </Link>
   );
 };
 
