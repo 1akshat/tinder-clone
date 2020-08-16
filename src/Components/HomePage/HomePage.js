fd08fe057f8495024c729b77cc70515986ca7ef8
1akshat <akshat.akshat6@gmail.com>
2020-08-16 13:43:47 +0530

    homepage component -> done

--- src/Components/HomePage/HomePage.js
+++ src/Components/HomePage/HomePage.js
@@ -1,10 +1,12 @@
 import React from "react";
 import TinderCards from "./TinderCards/TinderCards";
+import BottomSwipeButtons from "./BottomSwipeButtons/BottomSwipeButtons";
 
 const HomePage = () => {
   return (
     <>
-      <TinderCards />;
+      <TinderCards />
+      <BottomSwipeButtons />
     </>
   );
 };
