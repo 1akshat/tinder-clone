7a81e9de9658493f3b6fa5eefa9803e39e853750
1akshat <akshat.akshat6@gmail.com>
2020-08-16 12:27:05 +0530

    add more users to cards -> harcoded

--- src/Components/HomePage/HomePage.js
+++ src/Components/HomePage/HomePage.js
@@ -5,14 +5,34 @@ import "./HomePage.css";
 const HomePage = () => {
   const [people, setPeople] = useState([
     {
-      name: "Steve Jobs",
+      name: "Michel Joe",
       url:
-        "https://www.biography.com/.image/t_share/MTY2MzU3OTcxMTUwODQxNTM1/steve-jobs--david-paul-morrisbloomberg-via-getty-images.jpg",
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQxhRWVBVBcIINFDrTuCSNwm-QsnBOpOn3ybQ&usqp=CAU",
     },
     {
-      name: "Mark Zuckerberg",
+      name: "David Gandy",
       url:
-        "https://static.standard.co.uk/s3fs-public/thumbnails/image/2019/09/09/11/markzuckerberg.jpg",
+        "https://manofmany.com/wp-content/uploads/2019/03/10-Most-Famous-Male-Models-of-All-Time-David-Gandy.jpg",
+    },
+    {
+      name: "Mark Shawn",
+      url:
+        "https://c6oxm85c.cloudimg.io/cdno/n/q85/https://az617363.vo.msecnd.net/imgmodels/models/MD30001159/andreasdp119382a9c9d8dc05340a075edf137e150_thumb.jpg",
+    },
+    {
+      name: "Sawn Joe",
+      url:
+        "https://mediaslide-europe.storage.googleapis.com/named/pictures/768/1853/profile-1566992424-96f5a1800431801f3d48e0f68e506abe.jpg?v=1566992565",
+    },
+    {
+      name: "Bela Hedid",
+      url:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRLC82Stho0GRuZd4XIUf73yeP1sM0UlYpIRA&usqp=CAU",
+    },
+    {
+      name: "Mini Dsouza",
+      url:
+        "https://www.allperfectstories.com/wp-content/uploads/2017/06/MODEL-1024x640.jpg",
     },
   ]);
 
